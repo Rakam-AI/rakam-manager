294dac5a75939f7e4fcf5c7b0bfb12836d0624f9
MohammedJassem <mohammedjassemhlel@gmail.com>
2024-12-19 10:43:29 +0100

    update env automation

--- templates/rag_template/install.sh
+++ templates/rag_template/install.sh
@@ -3,11 +3,14 @@
 # Exit on error
 set -e
 
-pip install virtualenv
+# Get the parent folder name
+PARENT_FOLDER=$(basename "$(pwd)")
+
+# Install virtualenv if not already installed
+pip install --user virtualenv
 
 # Initialize a virtual environment
 python -m venv venv
 
-source ./venv/bin/activate
-
-pip install -r requirements.txt
+# Activate the virtual environment with a custom shell prompt
+exec bash --rcfile <(echo "source ./venv/bin/activate; export PS1='(${PARENT_FOLDER}_workspace) bash ';")
